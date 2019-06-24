86924bbc7daeb5a76bc6f3f2c6dbf796a6eedd4c
DJ Knox <dhknox@gmail.com>
2019-06-24 12:48:55 -0400

    add models, views, templates, and migrations for Book and Author

--- library/models.py
+++ library/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Author(models.Model):
+    """Model representing an author."""
+    name = models.CharField(max_length=200, help_text='Enter the name of the author')
+
+    # def get_absolute_url(self):
+    #     """Returns the url to access a particular author instance."""
+    #     return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Author object."""
+        return self.name
+
+
+class Book(models.Model):
+    """Model representing a book."""
+    title = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, help_text='Enter the URL of the book')
+    description = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+
+    # a book can only have one author, but authors can have multiple books
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        """String for representing the Book object."""
+        return self.title
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
--- library/views.py
+++ library/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from library.models import Book, Author
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of books and authors
+    num_books = Book.objects.all().count()
+    num_authors = Author.objects.count()
+    
+    context = {
+        'num_books': num_books,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
