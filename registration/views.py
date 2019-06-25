cd1090ad4b3c834a3964eb19c942169ad86e6b13
DJ Knox <dhknox@gmail.com>
2019-06-25 12:54:23 -0400

    add log_in_user_after_registering() to Register view to automatically log in a user after they are registered

--- registration/views.py
+++ registration/views.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import redirect
 
 
 class Register(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('index')
     template_name = 'registration/register.html'
+
+    # log in the user after registering
+    def form_valid(self, form):
+        return self.log_in_user_after_registering(form)
+
+    def log_in_user_after_registering(self, form):
+        form.save()
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'],)
+        login(self.request, user)
+        return redirect(self.success_url)
