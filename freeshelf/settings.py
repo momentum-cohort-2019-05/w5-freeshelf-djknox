188f89f0cefc8ce07fef5947eda4690e0ed8b70d
DJ Knox <dhknox@gmail.com>
2019-06-28 09:08:40 -0400

    re-order django-heroku settings in settings.py

--- freeshelf/settings.py
+++ freeshelf/settings.py
@@ -12,16 +12,16 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2h+*tjf5q$pk5%hjy8zpzgrb-)b8qv4rcz7c6zn%mk2*1r=^vj'
